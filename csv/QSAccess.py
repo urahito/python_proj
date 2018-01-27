6ea41176d5c43806d931748b80e9738d99524f02
urahito <solsky9103@gmail.com>
2018-01-27 14:57:24 +0900

    処理にコメント追加

--- csv/QSAccess.py
+++ csv/QSAccess.py
@@ -28,18 +28,26 @@ def main():
     file_dir, file_name = os.path.split(file_path)
     height = int(argv[2]) / 100.0
 
+    # csvをデータフレームに展開
     df = pd.read_csv(file_path, encoding='Shift_JIS', index_col='Start', engine='python')
 
+    # データフレーム上の列名を変更
     df = df.rename(columns={'Start':'日付', \
             'Body Fat Percentage (%)': '体脂肪率', 'Weight (kg)': '体重'})  
 
+    # データフレームの値列を体脂肪率と体重に絞る
     data = df[['体脂肪率', '体重']]
+
+    # 0は欠損値として扱い、そのレコードを削除する
     data = data[data > 0]
     data = data.dropna()
+
+    # BMI、除脂肪体重、基礎代謝(Katch-McArdle式:除脂肪体重から算出する式)を求める
     data['BMI'] = data['体重'] / (height ** 2)
     data['除脂肪体重'] = data['体重'] * (1 - data['体脂肪率'])
     data['基礎代謝'] = 370 + (21.6 * data['除脂肪体重'])
         
+    # 結果csvファイルに書き出す
     data.to_csv(os.path.join(file_dir, 'HealthData_stat.csv'))
     
 if __name__ == '__main__':
