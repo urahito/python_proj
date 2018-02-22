fa3783f0fa924532512f3a50a649b39b77b7351a
urahito <solsky9103@gmail.com>
2018-02-22 21:14:35 +0900

    ファイル取得をpathlib.openに変更

--- web/get_md_link.py
+++ web/get_md_link.py
@@ -4,36 +4,51 @@ import sys, bs4, requests, csv, os, codecs
 import unicodedata
 from pathlib import Path
 
-def main():
-    # 親ディレクトリの取得
-    parent_dir = Path(__file__).parent
+def remove_str(target, str_list):
+    return ''.join([c for c in target if c not in str_list])
 
-    # 入力、出力ファイル・ディレクトリの取得
-    files = list(parent_dir.joinpath('org').glob('*.txt'))
-    out_file = codecs.open(parent_dir.joinpath('out/result.csv'), 'w', 'sjis')
-    out_writer = csv.writer(out_file, dialect="excel")
+def ignore_str(target, enc):
+    return target.encode(enc, 'ignore').decode(enc)
 
-    # 入力ファイルでfor文を回す
-    for file_path in files:
-        # 指定ファイルをutf-8で開く
-        file_obj = open(file_path, 'r', encoding='utf-8')
-        # 全行取り込む
-        url_list = file_obj.readlines()
-
+def read_url(url_list, out_writer):
         # urlリストを1行ごとに処理する
         for url in url_list:
-            url = url.replace('\r', '').replace('\n', '')
+            url = remove_str(url, ['\r', '\n'])
 
             # ページの取得
             res = requests.get(url)
             soup = bs4.BeautifulSoup(res.text)
 
             # タイトルの取得とMarkdown用フォーマット
-            title = soup.select('title')[0].getText().encode('sjis', 'ignore').decode('sjis')
+            title = ignore_str(soup.title.string, 'sjis')
             markup = '[{}]({})'.format(title, url)
 
             # csvファイルへ書き出し
             out_writer.writerow([url, title, markup])
+
+def main():
+    # 各ディレクトリの取得
+    parent_dir = Path(__file__).parent
+    org_dir = parent_dir / 'org'
+    out_dir = parent_dir / 'out'
+
+    # フォルダが無ければ作成（あってもエラーなし）
+    org_dir.mkdir(exist_ok=True)
+    out_dir.mkdir(exist_ok=True)
+    out_file = out_dir / 'result.csv'
+
+    # 入力、出力ファイル・ディレクトリの取得
+    files = list(org_dir.glob('*.txt'))
+    with out_file.open('w', encoding='utf-8', newline='') as out_file_obj:
+        out_writer = csv.writer(out_file_obj, dialect="excel")
+
+        # 入力ファイルでfor文を回す
+        for file_path in files:
+            with Path(file_path).open('r', encoding='utf-8') as file_obj:
+                # 全行取り込む
+                url_list = file_obj.readlines()
+
+                read_url(url_list, out_writer)
 
 if __name__ == '__main__':
     main()
