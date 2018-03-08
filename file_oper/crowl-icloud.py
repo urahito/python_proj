e032839e185dbda4172958ad3d330f85cc5d59fb
urahito <solsky9103@gmail.com>
2018-03-09 06:17:53 +0900

    zip圧縮・移動バックアップスクリプト追加

---
+++ file_oper/crowl-icloud.py
@@ -0,0 +1,32 @@
+
+# zipファイルをDドライブの専用フォルダへ移動する
+def move_zip(zip_file, dest_dir):
+    #
+
+# 一時フォルダをzip化する
+def comp_to_zip(target_dir):
+    #
+
+# 特定したファイルを一時フォルダへ
+def move_to_temp_dir(files, target_dir):
+    #
+
+# 古いファイルを特定する(1年以上前なら1GBまで)
+def get_old_pictures(target_dir):
+    # 
+
+def main():
+    # iCloudフォルダを取得する(iniファイル)
+
+    # Dドライブの移動先フォルダを指定する(iniファイル)
+
+    # 古いファイルを特定する(1年以上前なら1GBまで)
+
+    # 特定したファイルを一時フォルダへ
+
+    # 一時フォルダをzip化する
+
+    # zipファイルをDドライブの専用フォルダへ移動する
+
+if __name__ == '__main__':
+    main()
