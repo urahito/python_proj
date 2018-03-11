8060bc7fc3f937e3ea68f57cb2909b625f6dab31
urahito <solsky9103@gmail.com>
2018-03-11 21:55:23 +0900

    ログcsvファイルの出力を追加

--- file_oper/iCloud/crowl-icloud.py
+++ file_oper/iCloud/crowl-icloud.py
@@ -3,37 +3,34 @@
 import configparser
 from operator import attrgetter
 from pathlib import Path
+import tqdm
 # 基本ライブラリ
 import sys, os
 import datetime
+import csv
 # 外部クラス
 sys.path.append(os.getcwd())
 from file_attr import file_attr
 
-# iniファイルから色々読み込む
-def get_ini_data(path_obj):
-    ini_file = configparser.SafeConfigParser()
+# ログ代わりの出力結果csvの出力
+def output_csv_log(files, input_dir, output_dir):
+    csv_name = 'result-{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    log_dir = output_dir / 'log'
+    log_dir.mkdir(exist_ok=True)
 
-    with path_obj.open('r', encoding='utf-8') as ini_file_obj:        
-        ini_file.read_file(ini_file_obj)
+    out_csv = log_dir / csv_name
+    print('log file: ' + str(out_csv))
 
-    return ini_file
+    with out_csv.open('w', encoding=file_attr.get_enc('w'), newline='') as out_file_obj:
+        csv_obj = csv.writer(out_file_obj, dialect='excel')
 
-def append_to_list(flist, input_dir, pattern_list):
-    for pattern in pattern_list:
-        for path in list(input_dir.glob(pattern)):
-            flist.append(path)
-    return flist
+        csv_obj.writerow(['対象フォルダ', input_dir])
+        csv_obj.writerow(['転送先フォルダ', output_dir])
+        csv_obj.writerow([''])
+        csv_obj.writerow(['ファイル名', '作成日時', '(KB)', '転送先フォルダ', '転送対象'])
 
-def get_files(input_dir, output_dir):
-    files = []
-    file_list = []
-    file_list = append_to_list(file_list, input_dir, ['*.gif', '*.PNG'])
-
-    for file_path in file_list:
-        files.append(file_attr(file_path, output_dir))
-
-    return sorted(files, key=attrgetter("create_time"))
+        for fi in tqdm.tqdm(files):
+            csv_obj.writerow(fi.output_line())
 
 # zipファイルをDドライブの専用フォルダへ移動する
 def move_zip(zip_file, dest_dir):
@@ -51,9 +48,37 @@ def move_to_temp_dir(files, target_dir):
 def get_old_pictures(files):
     size_sum = 0
     size_max = file_attr.get_big_size(500, 'MB')
+    found_ng = False
+
     for fi in files:
-        size_sum = fi.allow_file_copy(size_sum, size_max)
-        print(fi)
+        size_sum, found_ng = fi.allow_file_copy(size_sum, size_max, found_ng)
+
+# ファイルパターンを指定して、入力フォルダからのファイルを絞り込む
+def append_to_list(flist, input_dir, pattern_list):
+    for pattern in pattern_list:
+        for path in list(input_dir.glob(pattern)):
+            flist.append(path)
+    return flist
+
+# ファイル情報の取得
+def get_files(input_dir, output_dir):
+    files = []
+    file_list = []
+    file_list = append_to_list(file_list, input_dir, ['*.gif', '*.PNG'])
+
+    for file_path in tqdm.tqdm(file_list):
+        files.append(file_attr(file_path, output_dir))
+
+    return sorted(files, key=attrgetter("create_time"))
+
+# iniファイルから色々読み込む
+def get_ini_data(path_obj):
+    ini_file = configparser.SafeConfigParser()
+
+    with path_obj.open('r', encoding='utf-8') as ini_file_obj:        
+        ini_file.read_file(ini_file_obj)
+
+    return ini_file
 
 def main():
     # iniファイルの準備
@@ -71,11 +96,18 @@ def main():
     # ファイルの仕分け
     get_old_pictures(files)
     
-    # 特定したファイルを一時フォルダへ
+    # ファイル移動関係は、エラー時もログファイルを出力する
+    try:
+        print('')
+        # 特定したファイルを一時フォルダへ
 
-    # 一時フォルダをzip化する
+        # 一時フォルダをzip化する
 
-    # zipファイルをDドライブの専用フォルダへ移動する
+        # zipファイルをDドライブの専用フォルダへ移動する
+    finally:
+        # ログファイルの出力
+        output_csv_log(files, input_dir, output_dir)
+    print('finished')
 
 if __name__ == '__main__':
     main()
--- file_oper/iCloud/file_attr.py
+++ file_oper/iCloud/file_attr.py
@@ -11,7 +11,18 @@ class file_attr:
     @staticmethod
     def get_size_str(value, unit):
         unit_dic = dict(B=0, KB=1, MB=2, GB=3)
-        return value / (1024 ** unit_dic[unit])      
+        return value / (1024 ** unit_dic[unit])  
+
+    # 指定エンコードのgetメソッド
+    @staticmethod
+    def get_enc(mode):
+        enc_dic = dict(r='utf-8', w='sjis', p='cp932')
+        return enc_dic[mode]
+
+    # 指定エンコードでエラー文字以外を再取得する
+    @staticmethod
+    def ignore_str(target, enc):    
+        return target.encode(enc, 'ignore').decode(enc)  
     
     def __init__(self, fpath, dest_dir):
         org_path = Path(fpath)
@@ -28,28 +39,39 @@ class file_attr:
         self.dest_path = self.dest_org / org_path.name
         self.allow_copy = False
     
-    def allow_file_copy(self, sum_size, size_max):
+    def allow_file_copy(self, sum_size, size_max, past_ng):
+        # 既にNGが出ていないこと
+        if past_ng == True:
+            return sum_size, True
         # 存在するファイルであること
         if (self.org_path.exists() and self.org_path.is_file()) == False:
-            return sum_size
+            return sum_size, True
         # ファイルサイズがあること
         elif self.file_size <= 0:
-            return sum_size
+            return sum_size, True
         # 90日前までに作成されたファイルであること
         elif self.create_time >= datetime.datetime.today() - datetime.timedelta(days=90):
-            return sum_size
+            return sum_size, True
         # ファイルサイズの累計が上限値を超えないこと
         elif sum_size + self.file_size > size_max:
-            return sum_size
+            return sum_size, True
         
         self.allow_copy =  True
-        return sum_size + self.file_size
+        return (sum_size + self.file_size), False
     
     def set_sub_dir(self, sub_dir):
         self.dest_path = self.dest_org / sub_dir / self.org_path.name
 
+    def output_line(self):
+        org_name = self.ignore_str(str(self.org_path.name), self.get_enc('w'))  
+        dest_dir = self.ignore_str(str(self.dest_path.parent), self.get_enc('w'))  
+        create_str = self.create_time.strftime('%Y/%m/%d %H:%M:%S') 
+        size_str = str(int(self.get_size_str(self.file_size, 'KB')))
+
+        return [org_name, create_str, size_str, dest_dir, self.allow_copy]
+
     def __str__(self):
-        org_name = self.org_path.name     
+        org_name = self.org_path.name    
         create_str = self.create_time.strftime('%Y/%m/%d %H:%M:%S') 
         size_str = str(int(self.get_size_str(self.file_size, 'KB')))
 
@@ -57,5 +79,5 @@ class file_attr:
             .format(org_name, \
                     create_str, \
                     size_str, \
-                    self.dest_path.parent, \
-                    self.allow_copy)
+                    self.allow_copy, \
+                    self.dest_path.parent)
