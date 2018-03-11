9efb1d0b367376b4ce45e0022a88c62a82f5aa1a
urahito <solsky9103@gmail.com>
2018-03-11 15:51:24 +0900

    ファイル属性クラスを追加。表示にも対応

--- file_oper/iCloud/crowl-icloud.py
+++ file_oper/iCloud/crowl-icloud.py
@@ -1,8 +1,11 @@
+# -- coding: utf-8 --
 import configparser
+from operator import attrgetter
 import sys, os
 import time
 from pathlib import Path
-import file_attr
+sys.path.append(os.getcwd())
+from file_attr import file_attr
 
 # iniファイルから色々読み込む
 def get_ini_data(path_obj):
@@ -13,18 +16,21 @@ def get_ini_data(path_obj):
 
     return ini_file
 
-def get_files(input_dir):
-    files = {}
+def get_files(input_dir, output_dir):
+    files = []
     for gif_file in list(input_dir.glob('*.gif')):
-        files[gif_file] = time.localtime(os.path.getmtime(gif_file))
+        ftime = os.path.getmtime(gif_file)
+        fsize = os.path.getsize(gif_file)
+        files.append(file_attr(gif_file, ftime, fsize, output_dir))
 
     for png_file in list(input_dir.glob('*.PNG')):
-        files[png_file] = time.localtime(os.path.getmtime(png_file))
+        ftime = os.path.getmtime(png_file)
+        fsize = os.path.getsize(png_file)
+        files.append(file_attr(png_file, ftime, fsize, output_dir))
     
     print(str(len(files)) + '(flies)')
-    print(min(files.values()))
 
-    return sorted(files, key=lambda time: files.values())
+    return sorted(files, key=attrgetter("create_time"))
 
 # zipファイルをDドライブの専用フォルダへ移動する
 def move_zip(zip_file, dest_dir):
@@ -49,8 +55,12 @@ def main():
 
     # 古いファイルを特定する(1年以上前なら1GBまで)
     input_dir = Path(ini_data['settings']['input'])
+    output_dir = Path(ini_data['settings']['output'])
     print(input_dir)
-    files = get_files(input_dir)
+    files = get_files(input_dir, output_dir)
+
+    for fi in files:
+        print(fi)
     
     # 特定したファイルを一時フォルダへ
 
